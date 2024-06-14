52ac2960489b5b0f74e4764f6159a8d874370da8
s.gadekar <sgadekar@calibo.com>
2024-06-14 10:21:56 +0530

    title added

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -4,11 +4,14 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <div >
+    <Head>
+        <title>NextJS14</title>
     <nav style={{margin:'20px'}}>
       <Link href="/" style={{marginRight:'20px'}}>Home</Link>
       <Link href="/dashboard" style={{marginRight:'20px'}}>Dashboard</Link>
       <Link href="/listing">Listing</Link>
     </nav>
+    </Head>
       </div>
   );
 }
